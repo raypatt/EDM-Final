9d06d4d848de0a324693a1b1215505b3c11d86a1
Ray Patt <44343717+raypatt@users.noreply.github.com>
2022-05-28 15:59:01 -0400

    Update Sequence Analysis.R

--- Sequence Analysis.R
+++ Sequence Analysis.R
@@ -1,1 +1,10 @@
-expert_behaviors <- read.csv("~/Desktop/EDM Final Project/behaviors.csv")
+behaviors <- read.csv("~/Desktop/EDM Final Project/behaviors.csv")
+
+expert_string = levels(factor(behaviors$Class))[1]
+novice_string = levels(factor(behaviors$Class))[2]
+
+novices = subset(behaviors, Class == novice_string)
+expert = subset(behaviors, Class == expert_string)
+
+novices[order(-novices$Rate), ]
+head(expert[order(-expert$Rate), ])
